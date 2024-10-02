12c18ac4e52709a0a48ce1308967de5f1af0704d
Armando <armandocandelasalvarado@gmail.com>
2024-10-02 17:30:03 -0600

    registro viaje

--- html/dueño/registro/viaje.php
+++ html/dueño/registro/viaje.php
@@ -5,30 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../css/main.css">
     <title>Registrar Viaje</title>
+    <?php
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/nautiteq/php/php_dueno/registrar_viaje.php';
+    ?>
 </head>
 <body>
     <div class="header">
         <h1>Registrar Viaje</h1>
     </div>
     <div class="container">
-        <form id="registroViaje" onsubmit="guardarViaje(event)">
+        <form id="registroViaje" name="registrarViaje" method="post">
+            <div class="input-group">
+                <label for="barcoSeleccionado">Seleccionar Barco:</label>
+                <select id="barcoSeleccionado" name="barcoSeleccionado">
+                    <option value="">Seleccione...</option>
+                    <?php
+                    if (!empty($barcos)) {
+                        foreach ($barcos as $barco) {
+                            echo "<option value='" . $barco['barco_id'] . "'>" . $barco['denominacion'] . "</option>";
+                        }
+                    } else {
+                        echo "<option value=''>No hay barcos registrados</option>";
+                    }
+                    ?>
+                </select>
+            </div>
             <div class="input-group">
                 <label for="puertoOrigen">Puerto de Origen</label>
-                <input type="text" id="puertoOrigen" required>
+                <input type="text" id="puertoOrigen" name="puertoOrigen" required>
             </div>
             <div class="input-group">
                 <label for="puertoDestino">Puerto de Destino</label>
-                <input type="text" id="puertoDestino" required>
+                <input type="text" id="puertoDestino" name="puertoDestino" required>
             </div>
             <div class="input-group">
                 <label for="fechaInicio">Fecha de Inicio</label>
-                <input type="date" id="fechaInicio" required>
+                <input type="date" id="fechaInicio" name="fechaInicio" required>
             </div>
             <div class="input-group">
                 <label for="fechaFin">Fecha de Finalización</label>
-                <input type="date" id="fechaFin" required>
+                <input type="date" id="fechaFin" name="fechaFin" required>
             </div>
-            <button type="submit" class="button">Guardar</button>
+            <button type="submit" class="button" name="registrarViaje">Guardar</button>
         </form>
     </div>
 
